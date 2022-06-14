18dfe8bbbb2806c9a23704cb87c82cf821ecf4ee
Nate Moore <nate@astro.build>
2022-06-14 16:32:11 -0500

    chore: update playground

--- packages/playground/astro.config.mjs
+++ packages/playground/astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
-import topic from 'topic-dev';
+import topic from '@topic.dev/core';
 
 // https://astro.build/config
 export default defineConfig({
